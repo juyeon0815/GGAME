cf9e4577bdbc6ff4c6b36d683912660b4f6868c1
bibibic44 <bibibic@gmail.com>
2021-09-03 11:09:50 +0900

    FEAT: add argparse(do_sampling, sample_ratio)

--- config/config.py
+++ config/config.py
@@ -8,7 +8,14 @@ parser.add_argument('--caption_file_path', type=str, default='.\\datasets\\capti
 # 이미지 파일들이 저장된 경로
 parser.add_argument('--image_file_path', type=str, default='.\\datasets\\images')
 
+# Req. 3-4 do_sampling
+parser.add_argument('--do_sampling', action='store_true')
+parser.add_argument('sample_ratio', type=float)
+
 args = parser.parse_args()
 
+do_sampling = args.do_sampling
+sample_ratio = args.sample_ratio
+print(f'do_sampling: {do_sampling}')
 # print(args.caption_file_path)
 # print(args.image_file_path)
