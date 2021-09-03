02d46c65207336c66a1a3683a57b56ef2528f582
bibibic44 <bibibic@gmail.com>
2021-09-03 11:56:20 +0900

    FIX: add command line variable

--- config/config.py
+++ config/config.py
@@ -8,14 +8,25 @@ parser.add_argument('--caption_file_path', type=str, default='.\\datasets\\capti
 # 이미지 파일들이 저장된 경로
 parser.add_argument('--image_file_path', type=str, default='.\\datasets\\images')
 
+# Req. 3-2 val_ratio
+parser.add_argument('val_ratio', type=float)
+
+# Req. 3-3 dataset_path (path: train / val)
+parser.add_argument('dataset_path', type=str)
+
 # Req. 3-4 do_sampling
-parser.add_argument('--do_sampling', action='store_true')
-parser.add_argument('sample_ratio', type=float)
+# parser.add_argument('--do_sampling', action='store_true')
+parser.add_argument('--do_sampling', type=float)
+# parser.add_argument('sample_ratio', type=float)
 
 args = parser.parse_args()
 
-do_sampling = args.do_sampling
-sample_ratio = args.sample_ratio
+val_ratio = args.val_ratio
+dataset_path = args.dataset_path
+do_sampling = sample_ratio =args.do_sampling
+# sample_ratio = args.sample_ratio
+
+print(f'val_ratio: {val_ratio * 100}%')
 print(f'do_sampling: {do_sampling}')
 # print(args.caption_file_path)
 # print(args.image_file_path)
--- train.py
+++ train.py
@@ -14,11 +14,15 @@ img_paths, captions = preprocess.get_path_caption()
 
 
 # 전체 데이터셋을 분리해 저장하기 (val_ratio, dataset)
-train_dataset_path, val_dataset_path = preprocess.dataset_split_save(0.2, captions)
+train_dataset_path, val_dataset_path = preprocess.dataset_split_save(config.val_ratio, captions)
 
 
 # 저장된 데이터셋 불러오기 (path: train_dataset_path / val_dataset_path)
-img_paths, caption = preprocess.get_data_file(val_dataset_path)
+if config.dataset_path == 'train':
+    dataset_path = train_dataset_path
+else:
+    dataset_path = val_dataset_path
+img_paths, caption = preprocess.get_data_file(dataset_path)
 
 
 # 데이터 샘플링 (img_paths, caption, sample_ratio)
