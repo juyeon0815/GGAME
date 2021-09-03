086a93bb43edd1f95b3c7df46a6987f9b014735b
bibibic44 <bibibic@gmail.com>
2021-09-03 11:10:26 +0900

    FEAT: create Req function call

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
-# from config import config
-import config
+from config import config
 from data import preprocess
 from utils import utils
 
@@ -14,18 +13,18 @@ utils.config_read('caption_path')
 img_paths, captions = preprocess.get_path_caption()
 
 
-# 전체 데이터셋을 분리해 저장하기
-train_dataset_path, val_dataset_path = preprocess.dataset_split_save()
+# 전체 데이터셋을 분리해 저장하기 (val_ratio, dataset)
+train_dataset_path, val_dataset_path = preprocess.dataset_split_save(0.2, captions)
 
 
-# 저장된 데이터셋 불러오기
-img_paths, caption = preprocess.get_data_file()
+# 저장된 데이터셋 불러오기 (path: train_dataset_path / val_dataset_path)
+img_paths, caption = preprocess.get_data_file(val_dataset_path)
 
 
-# 데이터 샘플링
+# 데이터 샘플링 (img_paths, caption, sample_ratio)
 if config.do_sampling:
-    img_paths, caption = preprocess.sampling_data()
+    img_paths, caption = preprocess.sampling_data(img_paths, caption, config.sample_ratio)
 
 
 # 이미지와 캡션 시각화 하기
-utils.visualize_img_caption()
+utils.visualize_img_caption(img_paths, caption)
