6615c51bf1fea8014cb109d26ce00f4aaae1e5be
김주연 <wndusx1@gmail.com>
2021-09-28 20:14:10 +0900

    Docs: 대기실 글씨 가운데 정렬

--- frontend/src/Games/Pong/PongWaitingHost.js
+++ frontend/src/Games/Pong/PongWaitingHost.js
@@ -37,28 +37,34 @@ const PongWaitingHost = (props) =>{
     marginRight: '10px',
     width: '30%'
   }
+
+  const center = {
+    textAlign : 'center'
+  }
   return(
     <div>
-      <div>PongWaitingHost</div>
-
-      <div>
+      <h1>PongWaitingHost</h1>
+        
+        <div style={center}>
         <div>NAME : {nickName}</div>
+        {/* 여기에 pingpon게임이랑 webcam 컴포넌트 추가!!!! */}
         <div>
-        <input
-            type="text"
-            id="enter_room_host"
-            className="modal-input"
-            value={roomNumber}
-            ref={enterCode}
-            style={style}
-            disabled
+          <input
+              type="text"
+              id="enter_room_host"
+              className="modal-input"
+              value={roomNumber}
+              ref={enterCode}
+              style={style}
+              disabled
           />
           <button onClick={() => navigator.clipboard.writeText(enterCode.current.value)}>복사</button>
         </div>
+
         <div>{currentUser}명 대기중..
           <button onClick={gameStart}>게임시작</button>
         </div>
-      </div>
+        </div>
 
     </div>
   )
