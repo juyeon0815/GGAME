b4b87a70cc3491b13b90aa3282bb633193a7150d
bibibic44 <bibibic@gmail.com>
2021-09-29 09:55:28 +0900

    Fix: modify modal conflict

--- frontend/src/Games/SnakeGame/SnakeRule.js
+++ frontend/src/Games/SnakeGame/SnakeRule.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Modal } from '../../Mainpage/MainpageModal'
+import Modal from '../../Common/Modal'
 
 class SnakeRule extends React.Component {
   render() {
@@ -8,7 +8,7 @@ class SnakeRule extends React.Component {
       <Modal isOpen={isOpen} close={close} footerBtn="submit">
         <h1>게임 방법</h1>
         <p>1. 해당 위치를 클릭해서 상하좌우 버튼을 나만의 모션으로 등록하세요 ! 여러번 등록 시 인식률 증가합니다 ~!</p>
-        <p>2. 사과를 가장 많이 먹은 사람이 1위 !  뱀의 머리가 경계, 자신의 몸에 부딪치면 게임 오버</p>
+        <p>2. 사과를 가장 많이 먹은 사람이 1위 ! 뱀의 머리가 경계, 자신의 몸에 부딪치면 게임 오버</p>
       </Modal> 
     )
   }
