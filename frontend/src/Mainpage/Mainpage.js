f1081ce17cf1a7fb5f337b17a0e527bd263a7a35
hsh0321 <ppko1233@gmail.com>
2021-10-01 11:42:31 +0900

    fix : merge request

--- frontend/src/Mainpage/Mainpage.js
+++ frontend/src/Mainpage/Mainpage.js
@@ -1,32 +1,17 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Mainpage.css";
 import Pong from "../assets/images/pong.png";
 import Snake from "../assets/images/snake.png";
-import AirDrawing from "../assets/images/air_drawing.png";
 import GameCard from "./MainpageCard";
-import { faArrow, faMypage, faRanking } from "../assets/icon";
+import { faArrow, faMypage, faRanking } from "../assets/icons/menu_icon";
+import AirDrawing from "../assets/images/air_drawing.png";
 
-=======
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './Mainpage.css'
-import Pong from '../assets/images/pong.png'
-import Snake from '../assets/images/snake.png'
-import GameCard from './MainpageCard'
-import { faArrow, faMypage, faRanking } from '../assets/icons/menu_icon'
- 
->>>>>>> 967eb210bc3089f4425b0afd2239cd158f401090
 const Mainpage = () => {
   const user = "user";
   const [isMenu, setIsMenu] = useState(false);
   const visiblityChange = () => {
-<<<<<<< HEAD
     const icons = document.querySelectorAll(".icon-circle");
-=======
-    const icons = document.querySelectorAll('.icon-circle')
->>>>>>> 967eb210bc3089f4425b0afd2239cd158f401090
     if (isMenu) {
       setIsMenu(false);
       icons.forEach((icon) => {
@@ -60,13 +45,8 @@ const Mainpage = () => {
           {faRanking}
         </Link>
       </div>
-<<<<<<< HEAD
-      <div className={"container"}>
-        <GameCard image={Snake} game="snake" />
-=======
       <div className={"main-container"}>
-        <GameCard image={Snake} game="snake"/>
->>>>>>> 967eb210bc3089f4425b0afd2239cd158f401090
+        <GameCard image={Snake} game="snake" />
         <GameCard image={Pong} game="pong" />
         <GameCard image={AirDrawing} game="air-pong" />
       </div>
