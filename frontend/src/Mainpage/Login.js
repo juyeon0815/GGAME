04144a6a7405570d596f01bedd95b376522747bb
김주연 <wndusx1@gmail.com>
2021-10-04 00:12:32 +0900

    Feat: token유효성 검사 및 유저정보 반환

--- frontend/src/Mainpage/Login.js
+++ frontend/src/Mainpage/Login.js
@@ -1,12 +1,30 @@
 import Kakao from '../assets/images/kakao_login.png'
 import { kakao_auth_url } from './OAuth';
 
+import axios from 'axios';
+
 const Login = () =>{
 
+  function userMe(){
+    let token = sessionStorage.getItem('token')
+
+    alert("클릭")
+    axios.get("http://localhost:5000/user/me",{
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then((result)=>{
+      console.log(result)
+    })
+  }
+
   return(
-    <a href={kakao_auth_url}>
+    <div>
+      <a href={kakao_auth_url}>
       <img src={Kakao} alt="login"></img>
-    </a>
+      </a>
+      <button onClick={userMe}>회원정보</button>
+    </div>
   )
 }
 
