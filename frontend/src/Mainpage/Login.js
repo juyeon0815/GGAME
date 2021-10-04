519581dd7cd4989e08c7919f783b855e20bde5c7
bibibic44 <bibibic@gmail.com>
2021-10-04 14:01:00 +0900

    Feat: add login page

--- frontend/src/Mainpage/Login.js
+++ frontend/src/Mainpage/Login.js
@@ -1,12 +1,18 @@
 import Kakao from '../assets/images/kakao_login.png'
 import { kakao_auth_url } from './OAuth';
+import Logo from '../assets/images/Logo.png'
+import './Login.css'
 
-const Login = () =>{
-
-  return(
-    <a href={kakao_auth_url}>
-      <img src={Kakao} alt="login"></img>
-    </a>
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div>
+        <img src={Logo} alt='Logo' className="logo-style" />
+        <a href={kakao_auth_url} className="kakao-style">
+          <img src={Kakao} alt="login"></img>
+        </a>
+      </div>
+    </div>
   )
 }
 
