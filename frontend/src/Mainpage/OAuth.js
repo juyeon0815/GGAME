9c07c85fe7f7dc22063ef8c68097ae5931a680d1
dokyeong10 <dign1571@naver.com>
2021-10-05 00:15:53 +0900

    feat : deployment

--- frontend/src/Mainpage/OAuth.js
+++ frontend/src/Mainpage/OAuth.js
@@ -1,4 +1,4 @@
 const client_id = `fbb0739223a635262d2dcd957dd4c17c`
-const redirect_uri = `https://j5a104.p.ssafy.io:5000/callback/kakao`
+const redirect_uri = `https://j5a104.p.ssafy.io/callback/kakao`
 
 export const kakao_auth_url = `https://kauth.kakao.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`
