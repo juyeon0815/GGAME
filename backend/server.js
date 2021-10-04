2de4034b72b92ed5a461337eb18b9067adca0747
dokyeong10 <dign1571@naver.com>
2021-10-04 23:34:38 +0900

    feat : deployment

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const options ={
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // / 요청
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   console.log(__dirname);
   // index.html 파일 응답
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
