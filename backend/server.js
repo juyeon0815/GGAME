f3f4fe9d82f40dd4fbcf2c5d1122c9d8973ccedd
dokyeong10 <dign1571@naver.com>
2021-10-06 01:49:59 +0900

    Feat : deploy

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const https = require("https").createServer(options, app);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 //요청
-app.post("/", (req, res) => {
+app.get("/*", (req, res) => {
   console.log(__dirname);
   // index.html 파일 응답
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
