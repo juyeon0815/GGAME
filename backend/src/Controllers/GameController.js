d00324ed730a48126dee8dc69cb9a8388f8d3d7e
김주연 <wndusx1@gmail.com>
2021-10-05 21:02:13 +0900

    Feat: air-draw 게임결과/ 새로운 업적 달성 확인 api

--- backend/src/Controllers/GameController.js
+++ backend/src/Controllers/GameController.js
@@ -46,3 +46,27 @@ exports.newPongAchievement = async(req,res)=>{
         res.status(400).json({data:error})
     }
 }
+
+exports.airDrawGameResult = async(req,res)=>{
+    console.log(req.body)
+    try{
+        await GameService.airDrawGameResult(req.body.email,req.body.rank,req.body.score).then((result)=>{
+            console.log("result :", result)
+            res.status(200).json({data:result})
+        })
+    }catch(error){
+        res.status(400).json({data:error})
+    }
+}
+
+exports.newAirDrawAchievement = async(req,res)=>{
+    console.log(req.query.email, req.query.rank)
+    try{
+        await GameService.newAirDrawAchievement(req.query.email,req.query.rank).then((result)=>{
+            console.log("result :", result)
+            res.status(200).json({data: result})
+        })
+    }catch(error){
+        res.status(400).json({data: error})
+    }
+}
--- backend/src/Routes/Game.js
+++ backend/src/Routes/Game.js
@@ -7,4 +7,6 @@ router.get('/rank', GameController.getRank)
 router.post('/rank',GameController.newRank)
 router.get('/snake/new-achievement', GameController.newSnakeAchievement)
 router.get('/pong/new-achievement', GameController.newPongAchievement)
+router.post('/air-draw',GameController.airDrawGameResult)
+router.get('/air-draw/new-achievement',GameController.newAirDrawAchievement)
 module.exports = router;
