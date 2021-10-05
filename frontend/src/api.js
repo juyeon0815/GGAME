47ddb4fee552358131b7d35b2a17c6cbb5b56dc1
bibibic44 <bibibic@gmail.com>
2021-10-05 14:54:38 +0900

    Feat: add api methods

---
+++ frontend/src/api.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const UserMeApi = () => {
+  let token = sessionStorage.getItem('token')
+  axios.get("http://localhost:5000/user/me", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }).then(res => {
+    // console.log(res.data.data[0].email)
+    localStorage.setItem('email', res.data.data[0].email)
+  }).catch(err => {
+    console.log(err)
+    // alert('로그인 페이지로 이동합니다.')
+    // sessionStorage.removeItem('token')
+    // localStorage.removeItem('email')
+    // history.push({pathname: "/login"})
+  })
+}
+
+const getAchievement = () => {
+  let email = localStorage.getItem('email')
+  axios.get("http://localhost:5000/user/achievement", {
+    params: {
+      email: email
+    }
+  }).then(res => {
+    return res.data.data
+  }).catch(err => {
+    console.log(err)
+  })
+}
+  
+export {
+  UserMeApi,
+  getAchievement,
+}
