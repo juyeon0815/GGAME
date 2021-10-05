60df09e75a4d438426fe21c464132a472a7d6742
dokyeong10 <dign1571@naver.com>
2021-10-05 16:47:34 +0900

    fix :deploy

--- backend/src/Routes/User.js
+++ backend/src/Routes/User.js
@@ -5,5 +5,8 @@ const UserController = require('../Controllers/UserController')
 router.get('/auth/requestKakaoToken', UserController.getToken)
 router.get('/me',UserController.userMe)
 router.get('/achievement', UserController.getAchievement)
+router.get('/test', (req, res) => {
+    res.send('send test')
+})
 
 module.exports = router;
