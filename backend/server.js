439664e30f51ba852bbb2e3de41c748a33a3f77e
hsh0321 <ppko1233@gmail.com>
2021-10-05 13:36:23 +0900

    feat : server.js 수정

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,4 @@
-
-// const express = require('express')
-// const app = express()
-// const server = require('http').createServer(app)
-// const io = require('socket.io')(server,{
-//     cors : {
-//         origin :"*",
-//         credentials :true
-//         // origin: "http://localhost:3000",
-//         // methods: ["GET", "POST"],
-//     }
-// });
-
-// const cors = require('cors')
-// app.use(cors());
-
-// const Pong = require('./public/Pong/Pong')
-// // app.use('/pong',Pong)
-// Pong.pong(io,app)
-
-
-// const port = 5000;
-// server.listen(port, ()=>console.log(`Listening on port ${port}`));
-
-const express = require('express');
+const express = require("express");
 // const path = require("path"); // react build 파일에 접근하기 위해 필요함
 const port = process.env.PORT || 5000;
 
@@ -30,7 +6,6 @@ const app = express();
 
 const cors = require("cors");
 app.use(cors());
-
 
 // back/app.js
 const httpServer = require("http").createServer();
@@ -41,31 +16,24 @@ const io = require("socket.io")(httpServer, {
     methods: ["GET", "POST"],
   },
 });
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
-
-const pongModule = require("./public/AirDrawing/AirDrawing");
-// const charModule = require("./public/javascripts/Charade/Charade.js");
-const pongStateModule = require("./public/AirDrawing/AirDrawingState"); // 같은 디렉토리에 있다고 가정
+const airDrawingModule = require("./public/AirDrawing/AirDrawing");
+const airDrawingStateModule = require("./public/AirDrawing/AirDrawingState"); // 같은 디렉토리에 있다고 가정
 const requestPongModule = require("./public/AirDrawing/AirDrawingRequest"); // 같은 디렉토리에 있다고 가정
 
-const user = require('./src/Routes/User')
-const game = require('./src/Routes/Game')
-const achievement = require('./src/Routes/Achievement')
-app.use('/user',user)
-app.use('/game',game)
-app.use('/achievement',achievement)
+const user = require("./src/Routes/User");
+const game = require("./src/Routes/Game");
+const achievement = require("./src/Routes/Achievement");
+app.use("/user", user);
+app.use("/game", game);
+app.use("/achievement", achievement);
 
-pongModule.airDrawing(io,pongStateModule);
-// charModule.initChar(io,pongStateModule);
-
-requestPongModule.airDrawingRequest(app, pongStateModule);
-
-
-
+airDrawingModule.airDrawing(io, airDrawingStateModule);
+requestPongModule.airDrawingRequest(app, airDrawingStateModule);
 httpServer.listen(80);
 
 module.exports = app;
