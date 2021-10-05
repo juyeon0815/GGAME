9d8143bdb40fc536e973f03e7756d65068b8c93e
dokyeong10 <dign1571@naver.com>
2021-10-05 17:46:19 +0900

    fix :deploy

--- backend/src/Controllers/GameController.js
+++ backend/src/Controllers/GameController.js
@@ -6,7 +6,7 @@ exports.getRank =async(req,res)=>{
         await GameService.getRank(req.query.type).then((result)=>{
             res.status(200).json({data: result})
         })
-        console.log("try =====", data);
+        console.log("try =====");
     } catch (error) {
         console.log("========", error);
         res.status(400).json({data:error})
