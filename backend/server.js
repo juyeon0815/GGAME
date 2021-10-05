3e825e1dc59285c342aabc1c531e9e84ed89f23d
dokyeong10 <dign1571@naver.com>
2021-10-05 11:29:48 +0900

    feat : deployment

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ const pongModule = require("./public/AirDrawing/AirDrawing");
 const pongStateModule = require("./public/AirDrawing/AirDrawingState"); // 같은 디렉토리에 있다고 가정
 const requestPongModule = require("./public/AirDrawing/AirDrawingRequest"); // 같은 디렉토리에 있다고 가정
 
-const user = require('./src/Routes/user')
+const user = require('./src/Routes/User')
 const game = require('./src/Routes/Game')
 const achievement = require('./src/Routes/Achievement')
 
@@ -62,7 +62,6 @@ app.use('/achievement',achievement)
 
 pongModule.airDrawing(io,pongStateModule, options);
 // charModule.initChar(io,pongStateModule);
-
 requestPongModule.airDrawingRequest(app, pongStateModule, options);
 
 
